a88ce72793503b5fab48d64476ce96ba5c829885
Fernando <fernandoza@gmail.com>
2017-11-02 15:01:36 -0500

    Added controllers methods for each view

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -4,18 +4,24 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 // 1. Add controller annotation
 @Controller
 public class HomeController {
-    // 3. Add the response body annotation
-    @ResponseBody
-    // 4. Bind this method to a url pattern
-    @GetMapping("/")
+    @GetMapping("/hello/{name}")
     // 2. Create a regular method for your controller action
-    public String showLandingPage() {
-        return "This is the landing page!";
+    public String sayHello(@PathVariable String name, Model viewModel) {
+        // This is the replacement for the request method, in order to set attributes in the controller to the view.
+        viewModel.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/home")
+    public String showIndex() {
+        return "home";
     }
 }

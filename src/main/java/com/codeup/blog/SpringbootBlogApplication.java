56c379b7457fae808cd62768e59dc2af5de48a1f
Luis Montealegre <montealegreluis@gmail.com>
2017-11-02 09:34:36 -0500

    Create Spring application

---
+++ src/main/java/com/codeup/blog/SpringbootBlogApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogApplication.class, args);
+	}
+}

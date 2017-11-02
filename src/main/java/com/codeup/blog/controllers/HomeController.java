990841a0b0b473258b5a056fd1f98920845b4124
Luis Montealegre <montealegreluis@gmail.com>
2017-11-02 11:48:52 -0500

    Add method to show a message on the home page ("/")

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -3,5 +3,19 @@
  */
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// 1. Add controller annotation
+@Controller
 public class HomeController {
+    // 3. Add the response body annotation
+    @ResponseBody
+    // 4. Bind this method to a url pattern
+    @GetMapping("/")
+    // 2. Create a regular method for your controller action
+    public String showLandingPage() {
+        return "This is the landing page!";
+    }
 }

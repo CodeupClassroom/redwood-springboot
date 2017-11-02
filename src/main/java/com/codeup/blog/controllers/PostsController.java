46703b7736936d0278b00d92dbe903d2695df864
Fernando <fernandoza@gmail.com>
2017-11-02 13:52:59 -0500

    Solution for the controllers exercise.

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -3,5 +3,37 @@
  */
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostsController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String showAll(){
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String showPost(@PathVariable int id){
+        return "view an individual post " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String showCreateForm(){
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "create a new post";
+    }
+
 }
